effef7eb4738a7bc8b9084ced808f4a80c225b04
hyunah-kim <haha@huray.net>
2020-05-30 17:56:28 +0900

    Mustache UI - posts-save

--- src/main/java/my/study/hello/springboot/web/IndexController.java
+++ src/main/java/my/study/hello/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package my.study.hello.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String PostsSave() {
+        return "posts-save";
     }
 }
