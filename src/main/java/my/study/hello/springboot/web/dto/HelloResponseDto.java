f8db4de7103998a3b4368d61ac1146375b2caee8
HyunAh-iia <kha2464@gmail.com>
2020-06-07 19:07:01 +0900

    OAuth2 for Google

--- src/main/java/my/study/hello/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/my/study/hello/springboot/web/dto/HelloResponseDto.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter // create get methods of whole fields
-@RequiredArgsConstructor // create constructor with whole final fields
+@RequiredArgsConstructor // create constructor with whole final fields(No init.)
+                         // and create constructor with @Nullable fields
+//@AllArgsConstructor - create e constructor with whole fields
 public class HelloResponseDto {
 
     private final String name;
