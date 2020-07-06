49e88164085791711ec56f37bd3495e743328413
hyunah-kim <haha@huray.net>
2020-07-06 16:15:45 +0900

    Non-Stop Deployment : profile API

---
+++ src/main/java/my/study/hello/springboot/web/ProfileController.java
@@ -0,0 +1,30 @@
+package my.study.hello.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This controller choose which port(8081, 8082) using
+ */
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles()); // get ActiveProfile - real, oauth, real-db etc.
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2"); // "real" is for step2. it's optional.
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
